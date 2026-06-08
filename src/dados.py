fbeff16922fdaa52e31f6c3d5655a973b1f37771
Kemily <Kemilyeduardo240@gmail.com>
2026-06-08 20:53:47 -0300

    Atualização das cartas

--- src/dados.py
+++ src/dados.py
@@ -1,6 +1,7 @@
 import random
 
 def salvar_recorde(caminho_arquivo, pontuacao):
+    """Salva a pontuação recorde em arquivo texto."""
     with open(caminho_arquivo, "w", encoding="utf-8") as arquivo:
         arquivo.write(str(pontuacao))
 
@@ -22,17 +23,20 @@ cartas = []
 cartas_selecionadas = [] 
 
 def inicializar_tabuleiro():
+    """Gera as cartas, define suas posições na tela e embaralha"""
     global cartas, cartas_selecionadas
-    valores = [1, 1, 2, 2, 3, 3, 4, 4]
+    valores = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6]
     random.shuffle(valores)  
     
     coluna = 0
     linha = 0
     
     for valor in valores:
-        x = 70 + coluna * 120
-        y = 120 + linha * 120
+        """Calcula a posição X e Y de cada carta na tela"""
+        x = 100 + coluna * 120
+        y = 160 + linha * 120
 
+        """Cria o dicionário com os dados individuais da carta"""
         carta = {
             'id': valor,
             'x': x,
@@ -44,7 +48,8 @@ def inicializar_tabuleiro():
         }
         cartas.append(carta)
         
+        """Organiza o desenho em 4 colunas por linha"""
         coluna = coluna + 1
-        if coluna == 4:
+        if coluna == 6:
             coluna = 0
             linha = linha + 1
--- src/jogo.py
+++ src/jogo.py
@@ -1,6 +1,6 @@
 import pygame
 import sys
-import src.dados as dados  
+import src.dados as dados
 
 from src.config import (
     LARGURA_TELA,
@@ -10,67 +10,23 @@ from src.config import (
     FUNDO,
     CARTA,
     BOTAO,
-    TEXTO
+    TEXTO,
 )
 
-def detectar_clique(pos_mouse):
-    """Passa por todas as cartas para ver se o mouse clicou em alguma"""
-    for i in range(len(dados.cartas)):
-        carta = dados.cartas[i]
-
-        if carta['x'] <= pos_mouse[0] <= carta['x'] + carta['largura']:
-            if carta['y'] <= pos_mouse[1] <= carta['y'] + carta['altura']:
-                if not carta['virada'] and not carta['descoberta']:
-                    carta['virada'] = True
-                    dados.cartas_selecionadas.append(i) 
-
-def atualizar_jogo(tela):
-    if len(dados.cartas_selecionadas) == 2:
-        
-        desenhar_elementos(tela)
-        pygame.time.wait(800) 
-        
-        pos1 = dados.cartas_selecionadas[0]
-        pos2 = dados.cartas_selecionadas[1]
-        
-        carta1 = dados.cartas[pos1]
-        carta2 = dados.cartas[pos2]
-        
-        if carta1['id'] == carta2['id']:
-            carta1['descoberta'] = True
-            carta2['descoberta'] = True
-        else:
-            carta1['virada'] = False
-            carta2['virada'] = False
-
-        dados.cartas_selecionadas.clear()
-
-def desenhar_elementos(tela):
-    tela.fill(FUNDO)
-    fonte = pygame.font.SysFont("Arial", 40)
-    
-    for carta in dados.cartas:
-        if carta['virada'] or carta['descoberta']:
-            pygame.draw.rect(tela, (240, 240, 240), (carta['x'], carta['y'], carta['largura'], carta['altura']))
-            txt = fonte.render(str(carta['id']), True, TEXTO)
-            tela.blit(txt, (carta['x'] + 40, carta['y'] + 25))
-        else:
-            pygame.draw.rect(tela, CARTA, (carta['x'], carta['y'], carta['largura'], carta['altura']))
-            
-    pygame.display.update()
-
 def executar_jogo():
+    """Executa o loop principal do jogo: verificar eventos, desenhar tela, atualizar tela, controlar FPS"""
     pygame.init()
 
     tela = pygame.display.set_mode((LARGURA_TELA, ALTURA_TELA))
     pygame.display.set_caption(TITULO_JOGO)
 
     relogio = pygame.time.Clock()
-
+    
     dados.inicializar_tabuleiro()
 
     rodando = True
 
+    """Loop principal do jogo"""
     while rodando:
         relogio.tick(FPS)
 
@@ -81,16 +37,77 @@ def executar_jogo():
             if evento.type == pygame.KEYDOWN:
                 if evento.key == pygame.K_ESCAPE:
                     rodando = False
-            
-            # DETECTA CLIQUE DO MOUSE
-            if evento.type == pygame.MOUSEBUTTONDOWN:
-                if evento.button == 1: # Botão esquerdo
-                    detectar_clique(evento.pos)
                     
+            """"Detecta clique do mouse"""
+            if evento.type == pygame.MOUSEBUTTONDOWN:
+                if evento.button == 1: 
+                    detectar_clique(evento.pos)
+        
         atualizar_jogo(tela)
-
         desenhar_elementos(tela)
 
     pygame.quit()
+
+
+def detectar_clique(pos_mouse):
+    """Passa por todas as cartas para ver se o mouse clicou em alguma"""
+    for i in range(len(dados.cartas)):
+        carta = dados.cartas[i]
+        
+        """Verifica se o clique bateu dentro do quadrado da carta"""
+        if carta["x"] <= pos_mouse[0] <= carta["x"] + carta["largura"]:
+            if carta["y"] <= pos_mouse[1] <= carta["y"] + carta["altura"]:
+                """Garante que a carta só vira se estiver fechada"""
+                if not carta["virada"] and not carta["descoberta"]:
+                    carta["virada"] = True
+                    dados.cartas_selecionadas.append(i)
+
+
+def atualizar_jogo(tela):
+    """Verifica se o par de cartas escolhido é igual ou diferente"""
+    if len(dados.cartas_selecionadas) == 2:
+
+        desenhar_elementos(tela)
+        pygame.time.wait(800)
+
+        """Pega a posição das duas cartas que foram clicadas"""
+        pos1 = dados.cartas_selecionadas[0]
+        pos2 = dados.cartas_selecionadas[1]
+
+        carta1 = dados.cartas[pos1]
+        carta2 = dados.cartas[pos2]
+
+        """Se forem iguais, o jogador acertou o par"""
+        if carta1["id"] == carta2["id"]:
+            carta1["descoberta"] = True
+            carta2["descoberta"] = True
+        else:
+            carta1["virada"] = False
+            carta2["virada"] = False
+
+        dados.cartas_selecionadas.clear()
+
+
+def desenhar_elementos(tela):
+    """Desenha o fundo da janela e o estado atual de todas as cartas"""
+    tela.fill(FUNDO)
+    fonte = pygame.font.SysFont("Arial", 40)
+
+    for carta in dados.cartas:
+        """Desenha a carta aberta mostrando o seu número"""
+        if carta["virada"] or carta["descoberta"]:
+            pygame.draw.rect(
+                tela,
+                (240, 240, 240),
+                (carta["x"], carta["y"], carta["largura"], carta["altura"]),
+            )
+            txt = fonte.render(str(carta["id"]), True, TEXTO)
+            tela.blit(txt, (carta["x"] + 40, carta["y"] + 25))
+        else:
+            pygame.draw.rect(
+                tela, CARTA, (carta["x"], carta["y"], carta["largura"], carta["altura"])
+            )
+
+    pygame.display.update()
 
 executar_jogo()
