c5f51df8ebc7fd0907cd9a9a866f4fb07835af5d
Gabriel Hermont <gabrielsjh2007@gmail.com>
2026-06-14 22:52:43 -0300

    testes em test_logica.py

--- tests/test_logica.py
+++ tests/test_logica.py
@@ -1,5 +1,6 @@
 from src.funcoes import calcular_pontos, jogador_perdeu, limitar_valor
-
+from src.funcoes import somar_tentativa, reiniciar_jogo
+import src.dados as dados
 
 def test_calcular_pontos():
     """Deve somar corretamente os pontos atuais com os pontos ganhos."""
@@ -29,3 +30,17 @@ def test_limitar_valor_acima_do_maximo():
 def test_limitar_valor_dentro_do_intervalo():
     """Deve manter o valor original quando ele ja estiver no intervalo."""
     assert limitar_valor(50, 0, 100) == 50
+
+def test_reiniciar_jogo():
+    "Testa se, depois de reiniciar o jogo, as tentativas voltam a ser 0 e o tabuleiro volta a configuração inicial"
+    dados.inicializar_tabuleiro()
+    assert reiniciar_jogo(9) == 0
+    assert len(dados.cartas) == 12
+
+def test_somar_tentativa():
+    "Testa se a função somar_tentativa soma corretamente o número de tentativas"
+    assert somar_tentativa(2, 3) == 4
+
+def test_somar_tentativa_sem_duas_cartas():
+    "Testa se a função somar_tentativa não soma tentativas quando não há exatamente duas cartas selecionadas"
+    assert somar_tentativa(1, 3) == 3
